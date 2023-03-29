a7bab2b38a35ba14d68f628424c7af37ec5f1e21
George <george@butter.systemsbiology.net>
2023-03-29 13:11:07 -0700

    add export of metadata for athena, update datasync script, script to reorganize aws buckets

---
+++ aws_logs.py
@@ -0,0 +1,27 @@
+from google.oauth2 import service_account
+from google.cloud import storage
+from google.cloud import bigquery
+import json
+import pyarrow as pa
+import gcsfs
+import pandas as pd
+import io
+
+
+
+cred_file='/Users/george/idc/secure_files/idc-dev-etl-8b37b7f025c4.json'
+bucket='aws_request_logs'
+strt='not_processed'
+end='processed'
+project_id = 'idc-dev-etl'
+if __name__=='__main__':
+  with open(cred_file) as source:
+    info = json.load(source)
+    credentials = service_account.Credentials.from_service_account_info(info)
+    fs = gcsfs.GCSFileSystem(project=project_id, token=CRED_FILE)
+    file_list = fs.ls(bucket + '/' + strt)
+    for nm in file_list:
+      source_uri = "gs://{}".format(nm)
+      df = pd.read_csv(source_uri,token=cred_file)
+      ll=1
+
